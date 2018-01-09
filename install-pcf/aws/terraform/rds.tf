58344ad7f3251a5a1f084a3c7b749675e3203e69
vasantheligeti <vasanthkumar.eligeti@cognizant.com>
2018-01-09 19:13:45 +0530

    Update rds.tf

--- install-pcf/aws/terraform/rds.tf
+++ install-pcf/aws/terraform/rds.tf
@@ -1,6 +1,6 @@
 resource "aws_db_subnet_group" "rds_subnet_group" {
     name = "${var.prefix}-rds_subnet_group"
-    subnet_ids = ["${aws_subnet.PcfVpcRdsSubnet_az1.id}", "${aws_subnet.PcfVpcRdsSubnet_az2.id}", "${aws_subnet.PcfVpcRdsSubnet_az3.id}"]
+    subnet_ids = ["${aws_subnet.PcfVpcRdsSubnet_az1.id}"]
     tags {
         Name = "${var.prefix} RDS DB subnet group"
     }
