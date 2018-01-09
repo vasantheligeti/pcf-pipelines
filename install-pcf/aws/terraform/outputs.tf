1e92100e4a998a7ad71f68350424df785b542068
vasantheligeti <vasanthkumar.eligeti@cognizant.com>
2018-01-09 16:21:28 +0530

    Update outputs.tf

--- install-pcf/aws/terraform/outputs.tf
+++ install-pcf/aws/terraform/outputs.tf
@@ -7,12 +7,6 @@ output "region" {
 output "az1" {
     value = "${var.aws_az1}"
 }
-output "az2" {
-    value = "${var.aws_az2}"
-}
-output "az3" {
-    value = "${var.aws_az3}"
-}
 output "vpc_id" {
     value = "${aws_vpc.PcfVpc.id}"
 }
@@ -110,96 +104,6 @@ output "infra_subnet_gw_az1" {
     value = "${cidrhost("${var.infra_subnet_cidr_az1}", 1)}"
 }
 
-
-# AZ2
-
-output "public_subnet_cidr_az2" {
-    value = "${var.public_subnet_cidr_az2}"
-}
-output "ert_subnet_cidr_az2" {
-    value = "${var.ert_subnet_cidr_az2}"
-}
-output "ert_subnet_gw_az2" {
-    value = "${cidrhost("${var.ert_subnet_cidr_az2}", 1)}"
-}
-
-output "rds_subnet_cidr_az2" {
-    value = "${var.rds_subnet_cidr_az2}"
-}
-output "services_subnet_cidr_az2" {
-    value = "${var.services_subnet_cidr_az2}"
-}
-output "services_subnet_gw_az2" {
-    value = "${cidrhost("${var.services_subnet_cidr_az2}", 1)}"
-}
-output "dynamic_services_subnet_cidr_az2" {
-    value = "${var.dynamic_services_subnet_cidr_az2}"
-}
-output "dynamic_services_subnet_gw_az2" {
-    value = "${cidrhost("${var.dynamic_services_subnet_cidr_az2}", 1)}"
-}
-output "public_subnet_id_az2" {
-    value = "${aws_subnet.PcfVpcPublicSubnet_az2.id}"
-}
-output "ert_subnet_id_az2" {
-    value = "${aws_subnet.PcfVpcErtSubnet_az2.id}"
-}
-output "rds_subnet_id_az2" {
-    value = "${aws_subnet.PcfVpcRdsSubnet_az2.id}"
-}
-output "services_subnet_id_az2" {
-    value = "${aws_subnet.PcfVpcServicesSubnet_az2.id}"
-}
-output "dynamic_services_subnet_id_az2" {
-    value = "${aws_subnet.PcfVpcDynamicServicesSubnet_az2.id}"
-}
-
-#AZ3
-
-output "public_subnet_cidr_az3" {
-    value = "${var.public_subnet_cidr_az3}"
-}
-output "ert_subnet_cidr_az3" {
-    value = "${var.ert_subnet_cidr_az3}"
-}
-
-output "ert_subnet_gw_az3" {
-    value = "${cidrhost("${var.ert_subnet_cidr_az3}", 1)}"
-}
-
-output "rds_subnet_cidr_az3" {
-    value = "${var.rds_subnet_cidr_az3}"
-}
-output "services_subnet_cidr_az3" {
-    value = "${var.services_subnet_cidr_az3}"
-}
-output "dynamic_services_subnet_cidr_az3" {
-    value = "${var.dynamic_services_subnet_cidr_az3}"
-}
-
-output "public_subnet_id_az3" {
-    value = "${aws_subnet.PcfVpcPublicSubnet_az3.id}"
-}
-output "services_subnet_gw_az3" {
-    value = "${cidrhost("${var.services_subnet_cidr_az3}", 1)}"
-}
-output "dynamic_services_subnet_gw_az3" {
-    value = "${cidrhost("${var.dynamic_services_subnet_cidr_az3}", 1)}"
-}
-output "ert_subnet_id_az3" {
-    value = "${aws_subnet.PcfVpcErtSubnet_az3.id}"
-}
-output "rds_subnet_id_az3" {
-    value = "${aws_subnet.PcfVpcRdsSubnet_az3.id}"
-}
-output "services_subnet_id_az3" {
-    value = "${aws_subnet.PcfVpcServicesSubnet_az3.id}"
-}
-output "dynamic_services_subnet_id_az3" {
-    value = "${aws_subnet.PcfVpcDynamicServicesSubnet_az3.id}"
-}
-
-# RDS info
 
 output "db_host" {
     value = "${aws_db_instance.pcf_rds.address}"
